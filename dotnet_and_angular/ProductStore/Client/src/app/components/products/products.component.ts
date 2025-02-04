d1bf8a2c00f1b8bbae11a2db77ce60182c9a1305
Jalen Meinke <meinkej1@students.westerntc.edu>
2025-02-04 10:38:45 -0600

    Mid class push

--- dotnet_and_angular/ProductStore/Client/src/app/components/products/products.component.ts
+++ dotnet_and_angular/ProductStore/Client/src/app/components/products/products.component.ts
@@ -1,21 +1,24 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent implements OnInit {
-
   private _productService = inject(ProductService);
 
-  ngOnInit(): void {
+  // By virtue of having this & how the getAllProducts() is implemented,
+  // we will have access to all products once our http call successfully returns.
+  public productList$ = this._productService.productList$;
+
+  ngOnInit(): void {    
     this._productService.getAllProducts().subscribe((res) => {
-      console.log("Res:", res);
-    })
+      console.log('res:', res);
+    });
   }
 
 }
