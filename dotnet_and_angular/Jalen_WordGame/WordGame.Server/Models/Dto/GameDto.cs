bde4ad93be619db2c2a4671811b51f28594686ab
Jalen Meinke <meinkej1@students.westerntc.edu>
2025-04-06 08:19:57 -0500

    Matched gameplay controller and gamedto

--- dotnet_and_angular/Jalen_WordGame/WordGame.Server/Models/Dto/GameDto.cs
+++ dotnet_and_angular/Jalen_WordGame/WordGame.Server/Models/Dto/GameDto.cs
@@ -3,7 +3,7 @@ public class GameDto
     public int Id { get; set; }
     public string? UserId { get; set; }
     public string? Status { get; set; }
-    public string? View { get; set; } // This is the 'phrase' in the frontend.
+    public string? Phrase { get; set; } 
     public int RemainingGuesses { get; set; }
-    public string? Answer { get; set; } // Added Answer for the game
+    public string? Answer { get; set; }
 }
