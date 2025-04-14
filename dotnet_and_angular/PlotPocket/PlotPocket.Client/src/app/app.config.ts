eda0f07be85a590802cb247c662b5d7bff69ad9a
Jalen Meinke <meinkej1@students.westerntc.edu>
2025-04-14 11:36:51 -0500

    Started fixing cards

--- dotnet_and_angular/PlotPocket/PlotPocket.Client/src/app/app.config.ts
+++ dotnet_and_angular/PlotPocket/PlotPocket.Client/src/app/app.config.ts
@@ -1,23 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-<<<<<<< HEAD
-import { provideHttpClient } from '@angular/common/http';
-import { TrendingService } from './services/trending.service';
-
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-    TrendingService,  // Add TrendingService here to provide it globally
-  ],
-=======
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideHttpClient()],
->>>>>>> f6772b669156dbd79d36cc6622c6623a3ca220b6
 };
