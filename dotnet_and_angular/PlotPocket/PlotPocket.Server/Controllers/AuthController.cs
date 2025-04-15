5a8ab8274dce22764450fbf1b0f66ddeec6236da
Jalen Meinke <meinkej1@students.westerntc.edu>
2025-04-15 15:58:47 -0500

    Finsihed Login

--- dotnet_and_angular/PlotPocket/PlotPocket.Server/Controllers/AuthController.cs
+++ dotnet_and_angular/PlotPocket/PlotPocket.Server/Controllers/AuthController.cs
@@ -11,12 +11,12 @@ namespace PlotPocket.Server.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
         public AuthController(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager
         )
         {
             _userManager = userManager;
